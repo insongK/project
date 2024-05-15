0ced26c43ef20957e28720a23c3f00600804538b
insonggim <insonggim8@gmail.com>
2024-05-15 21:25:27 +0900

    회원가입 post 형식 추가, 및 body-parser 적용

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const dbconfig = require("./config/database.js");
+const bodyparser = require("body-parser");
 const connection = mysql.createConnection(dbconfig);
 
 const app = express();
@@ -8,24 +9,28 @@ const app = express();
 // configuration =========================
 app.set("port", process.env.PORT || 3000);
 
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.use(bodyparser.json());
+
 app.get("/", (req, res) => {
   res.send("Root");
 });
 
 app.get("/users", (req, res) => {
-  connection.query("SELECT * from Users", (error, rows) => {
+  connection.query("SELECT * from users", (error, rows) => {
     if (error) throw error;
     console.log("User info is: ", rows);
     res.send(rows);
   });
 });
 
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
 
   // 여기서는 간단한 예제로 사용자를 users 테이블에 저장합니다.
-  const sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-  connection.query(sql, [username, password], (err, results) => {
+  const sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+  connection.query(sql, [name, email, password], (err, results) => {
     if (err) {
       console.error("Error saving user data to MySQL: " + err.stack);
       res.status(500).send("Error saving user data to MySQL");
